6f6947632c67e663b87e84500590e0bef871202f
Subhadeep <subhadeep0326@gmail.com>
2021-10-09 11:42:36 +0530

    [Subhadeep employee wage 20 days UC5 commit]

--- EmployeeWage01/Program.cs
+++ EmployeeWage01/Program.cs
@@ -4,39 +4,53 @@ namespace EmployeeWage01
 {
     class Program
     {
+        /// <summary>
+        /// The is full time
+        /// </summary>
+        
+        //Declaring constant variables
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to the Employee Wage computation program");            
             //Variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random(); //Random object creation
+            int totalEmpWage = 0;
+            int day;
 
-            //Computation
-            int empCheck = random.Next(0, 3);
-           switch (empCheck)
+            for (day = 1; day <= NUM_OF_WORKING_DAYS; day++)  //for loop to iterate through 20 days
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    Console.WriteLine("Employee is Part time");
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    Console.WriteLine("Employee is Fulltime");
-                    break;
-                default:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is Absent");
-                    break;
+
+                Random random = new Random(); //Random object creation
+
+                //Computation
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        //Console.WriteLine("Employee is Part time");
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        //Console.WriteLine("Employee is Fulltime");
+                        break;
+                    default:
+                        empHrs = 0;
+                        //Console.WriteLine("Employee is Absent");
+                        break;
 
 
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee Wage: {0} and Day: {1} ", empWage, day);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage: " + empWage);
-            
+            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
         }
     }
 }
