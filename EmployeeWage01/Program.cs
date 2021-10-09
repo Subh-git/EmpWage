2ac83751e299a8781241c3c922e70318b9edae85
Subhadeep <subhadeep0326@gmail.com>
2021-10-09 11:02:17 +0530

    [Subhadeep UC1 commit]

--- EmployeeWage01/Program.cs
+++ EmployeeWage01/Program.cs
@@ -6,8 +6,23 @@ namespace EmployeeWage01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage computation program");
-            Console.ReadLine();
+            //Console.WriteLine("Welcome to the Employee Wage computation program");
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            
+
         }
     }
 }
