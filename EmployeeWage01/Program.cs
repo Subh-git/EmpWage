188b20ed19558f0cdf1006f8c1537bc6237678b8
Subhadeep <subhadeep0326@gmail.com>
2021-10-09 11:10:04 +0530

    [Subhadeep empWage UC2 commit]

--- EmployeeWage01/Program.cs
+++ EmployeeWage01/Program.cs
@@ -9,20 +9,28 @@ namespace EmployeeWage01
             //Console.WriteLine("Welcome to the Employee Wage computation program");
             //Constants
             int IS_FULL_TIME = 1;
-            Random random = new Random();
+            int EMP_RATE_PER_HOUR = 20;
+
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random(); //Random object creation
 
             //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage: " + empWage);
             
-
         }
     }
 }
