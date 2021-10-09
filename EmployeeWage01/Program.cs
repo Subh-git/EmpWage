e320927e9a02c5ef5a612d94b4484749d7f23db8
Subhadeep <subhadeep0326@gmail.com>
2021-10-09 10:10:18 +0530

    [Subhadeep Emp First commit]

---
+++ EmployeeWage01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWage01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage computation program");
+            Console.ReadLine();
+        }
+    }
+}
