1478876a398d8fbe9a511c64673f1722879a9815
Subhadeep <subhadeep0326@gmail.com>
2021-10-09 11:17:48 +0530

    [Subhadeep Employee wage part time UC3 commit]

--- EmployeeWage01/Program.cs
+++ EmployeeWage01/Program.cs
@@ -9,19 +9,26 @@ namespace EmployeeWage01
             //Console.WriteLine("Welcome to the Employee Wage computation program");
             //Constants
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-
+            
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random(); //Random object creation
 
             //Computation
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
+            }
+
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part time");
+                empHrs = 4;
             }
             else
             {
