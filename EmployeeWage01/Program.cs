4bc5b65f8d44ee1abbbad2c64970758fb6d44c4b
Subhadeep <subhadeep0326@gmail.com>
2021-10-09 11:28:01 +0530

    [Subhadeep employee switch case UC4]

--- EmployeeWage01/Program.cs
+++ EmployeeWage01/Program.cs
@@ -4,14 +4,12 @@ namespace EmployeeWage01
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome to the Employee Wage computation program");
-            //Constants
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            
+            //Console.WriteLine("Welcome to the Employee Wage computation program");            
             //Variables
             int empHrs = 0;
             int empWage = 0;
@@ -19,21 +17,22 @@ namespace EmployeeWage01
 
             //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+           switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-            }
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Part time");
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    Console.WriteLine("Employee is Fulltime");
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
 
-            else if (empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part time");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage: " + empWage);
