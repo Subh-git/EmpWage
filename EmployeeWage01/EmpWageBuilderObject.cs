bf5742767b26e9178020435ab5df66b9ee624ba2
Subhadeep <subhadeep0326@gmail.com>
2021-10-27 20:21:42 +0530

    Subhadeep added multiple company details with interface approach

--- EmployeeWage01/EmpWageBuilderObject.cs
+++ EmployeeWage01/EmpWageBuilderObject.cs
@@ -6,27 +6,44 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage01
 {
-    public class EmpWageBuilderArray
+    public interface IComputeEmpWage                                 //declaration of the interface class
+    {
+        /// <summary>
+        /// Adds the company emp wage.
+        /// </summary>
+        /// <param name="companyName">Name of the company.</param>
+        /// <param name="maxWorkingDay">The maximum working day.</param>
+        /// <param name="maxWorkingHrs">The maximum working HRS.</param>
+        /// <param name="empRate">The emp rate.</param>
+        public void addCompanyEmpWage(string companyName, int maxWorkingDay, int maxWorkingHrs, int empRate);
+        public void computeEmpWage();
+
+
+    }
+    public class EmpWageBuilderArray : IComputeEmpWage                     //inheriting from the interfce class
     {
         //constants
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
 
-        private int numOfCompany = 0;
-        private CompanyEmpWage[] companyEmpWageArray;
+        private int numOfCompany = 0;                                    //works as the pointer for array(index)
+        private CompanyEmpWage[] companyEmpWageArray;                    //initialising the array of the referrence type CompanyEmpWage class which holds 
+                                                                         //all the data about the various variables in it.
 
-        public EmpWageBuilderArray()                                //declaring the array size and all
+        public EmpWageBuilderArray()                                    //declaring the array size 
         {
             this.companyEmpWageArray = new CompanyEmpWage[5];
         }
 
         //method to insert into the array
-        public void addCompanyEmpWage(string companyName, int maxWorkingDay, int maxWorkingHrs, int empRate) 
+        public void addCompanyEmpWage(string companyName, int maxWorkingDay, int maxWorkingHrs, int empRate)  //completeing the interfaced method
         {
-            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(companyName, maxWorkingDay, maxWorkingHrs, empRate);
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(companyName, maxWorkingDay, maxWorkingHrs, empRate);    //incrementing the array
             numOfCompany++;
+
+            //one array index element/object can be used to call mutilple variables of CompanyEmpWage class  
         }
-        public void computeEmpWage()
+        public void computeEmpWage()                                                     //similar method like compute emp wage just that it fethces a single element and then finds out its respected emp wage(total)
         {
             for (int i = 0; i < numOfCompany; i++)
             {
@@ -35,7 +52,7 @@ namespace EmployeeWage01
             }
         }
 
-        private int ComputeEmpWage(CompanyEmpWage Company)
+        private int ComputeEmpWage(CompanyEmpWage Company)                                  //the main computing method
         {
             int empWage = 0;
             int totalWorkingDay = 0;
@@ -48,7 +65,7 @@ namespace EmployeeWage01
                 //used predefined Random class
                 Random attendance = new Random();
                 int empInput = attendance.Next(0, 3);
-
+                //various switch cases to access theempInput and change the emp hours accordingly
                 switch (empInput)
                 {
                     case IS_FULL_TIME:
@@ -103,7 +120,7 @@ namespace EmployeeWage01
         {
             this.totalEmpWage = totalEmpWage;
         }
-        public string toString()
+        public string toString()                     //this method returns the total emp wage of the respected company in string format 
         {
             return "Total Employee Wage For " + this.companyName + " " + "is " + this.totalEmpWage;
         }
